3196d84f134a7ed0a7b73fca5413aa2519ba6e35
Stacia <ixximm.11820@gmail.com>
2021-08-18 16:36:12 +0700

    just update

--- utils/embed_converter.py
+++ utils/embed_converter.py
@@ -7,11 +7,11 @@ def set_channel_embed(ctx):
     embed = Embed(title="Set channel", color=0xffffff)
 
     embed.add_field(name="\u200b" , value="**WELCOME**" , inline=False)
-    embed.add_field(name="welcome channel" , value="```lt welcome set [channel]```" , inline=False)
+    embed.add_field(name="welcome channel" , value="```lt set welcome [channel]```" , inline=False)
     embed.add_field(name="Delete welcome channel" , value="```lt welcome delete```" , inline=False)
 
     embed.add_field(name="\u200b" , value="\u200b**LEAVE**" , inline=False)
-    embed.add_field(name="leave channel" , value="```lt leave set [channel]```" , inline=False)
+    embed.add_field(name="leave channel" , value="```lt set leave [channel]```" , inline=False)
     embed.add_field(name="Delete leave channel" , value="```lt leave delete```" , inline=False)
     
     return embed
